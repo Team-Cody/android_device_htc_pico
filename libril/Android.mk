7ff5d1acf3d087c550d96d188ab90e2dcbdc300c
sakindia123 <sa6915@gmail.com>
2013-03-13 20:34:23 +0530

    libril: Fix derp

--- libril/Android.mk
+++ libril/Android.mk
@@ -1,6 +1,8 @@
 # Copyright 2006 The Android Open Source Project
 
-ifneq ($(BOARD_PROVIDES_LIBRIL),true)
+ifeq ($(BOARD_PROVIDES_LIBRIL),true)
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
+ifeq ($(BOARD_VENDOR),htc)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
@@ -16,44 +18,13 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware_legacy
 
 LOCAL_CFLAGS :=
-ifdef BOARD_USE_NEW_LIBRIL_HTC
-    LOCAL_CFLAGS += -DNEW_LIBRIL_HTC
-endif
 
 LOCAL_MODULE:= libril
 
 LOCAL_LDLIBS += -lpthread
 
-#USE HCRADIO
-ifeq ($(BOARD_USES_HC_RADIO),true)
-LOCAL_CFLAGS += -DHCRADIO
-endif
-
-ifeq ($(BOARD_USES_LEGACY_RIL),true)
-LOCAL_CFLAGS += -DLEGACY_RIL
-endif
-
 include $(BUILD_SHARED_LIBRARY)
 
-
-# For RdoServD which needs a static library
-# =========================================
-ifneq ($(ANDROID_BIONIC_TRANSITION),)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= \
-    ril.cpp
-
-LOCAL_STATIC_LIBRARIES := \
-    libutils_static \
-    libcutils
-
-LOCAL_CFLAGS :=
-
-LOCAL_MODULE:= libril_static
-
-LOCAL_LDLIBS += -lpthread
-
-include $(BUILD_STATIC_LIBRARY)
-endif # ANDROID_BIONIC_TRANSITION
+endif # BOARD_VENDOR
+endif # TARGET_BOARD_PLATFORM
 endif # BOARD_PROVIDES_LIBRIL
